98e86b175fc905d3cfb36eedfac4bff294872d05
Matvey Kham <mvham@mail.ru>
2022-06-28 21:17:03 +0700

    add joi in routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
 
 const {
   getUsers,
@@ -9,9 +10,42 @@ const {
 } = require("../controllers/users");
 
 router.get("/", getUsers);
+
 router.get("/me", getMe);
-router.get("/:userId", getUserById);
-router.patch("/me", updateUserProfile);
-router.patch("/me/avatar", updateUserAvatar);
+
+router.get(
+  "/:userId",
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().length(24).hex().required(),
+    }),
+  }),
+  getUserById
+);
+
+router.patch(
+  "/me",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  updateUserProfile
+);
+
+router.patch(
+  "/me/avatar",
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string()
+        .pattern(
+          /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/
+        )
+        .required(),
+    }),
+  }),
+  updateUserAvatar
+);
 
 module.exports = router;
